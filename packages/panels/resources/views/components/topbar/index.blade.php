1cde8b2c6943ed76c30b01c68cd9b636ad3deaec
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2024-11-08 17:51:44 +0100

    fix: global search

--- packages/panels/resources/views/components/topbar/index.blade.php
+++ packages/panels/resources/views/components/topbar/index.blade.php
@@ -158,7 +158,7 @@
             {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::GLOBAL_SEARCH_BEFORE) }}
 
             @if (filament()->isGlobalSearchEnabled())
-                @livewire(Filament\Livewire\GlobalSearch::class)
+                @livewire(Filament\Livewire\GlobalSearch::class, key('global-search' . (filament()->hasTenancy() ? '-' . filament()->getTenant()->getKey() : '')))
             @endif
 
             {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::GLOBAL_SEARCH_AFTER) }}
