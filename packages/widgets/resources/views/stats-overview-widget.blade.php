eeb4f6ae700fe9bc6c72d3c701300ccfe7d1ccd4
Ryan Chandler <ryangjchandler@gmail.com>
2024-11-13 13:10:33 +0000

    Widgets/StatsOverview: Add support for heading and description

--- packages/widgets/resources/views/stats-overview-widget.blade.php
+++ packages/widgets/resources/views/stats-overview-widget.blade.php
@@ -1,8 +1,26 @@
 @php
     $columns = $this->getColumns();
+    $hasHeading = $this->hasHeading();
+    $hasDescription = $this->hasDescription();
 @endphp
 
 <x-filament-widgets::widget class="fi-wi-stats-overview">
+    @if($hasHeading || $hasDescription)
+        <div class="fi-wi-stats-overview-header grid flex-1 gap-y-1 mb-4">
+            @if($hasHeading)
+                <h3 class="fi-wi-stats-overview-header-heading text-base font-semibold leading-6 text-gray-950 dark:text-white col-span-full">
+                    {{ $this->getHeading() }}
+                </h3>
+            @endif
+
+            @if($hasDescription)
+                <p class="fi-wi-stats-overview-header-description overflow-hidden break-words text-sm text-gray-500 dark:text-gray-400">
+                    {{ $this->getDescription() }}
+                </p>
+            @endif
+        </div>
+    @endif
+
     <div
         @if ($pollingInterval = $this->getPollingInterval())
             wire:poll.{{ $pollingInterval }}
