c43146596e77613f6d5b1745c5e4ef406994d9d8
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2024-12-09 19:24:40 +0100

    Style

--- tests/src/MultiDomainPanel.php
+++ tests/src/MultiDomainPanel.php
@@ -18,7 +18,6 @@ use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
-use Filament\Http\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
