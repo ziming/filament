8f0fe7f5ea1269deea0ac7fafcdc6c3f09f7f0ad
Dan Harrin <git@danharrin.com>
2024-12-31 13:10:31 +0000

    Update & rebuild

--- packages/forms/resources/lang/pl/validation.php
+++ packages/forms/resources/lang/pl/validation.php
@@ -4,7 +4,7 @@ return [
 
     'distinct' => [
         'must_be_selected' => 'Przynajmniej jedno pole :attribute musi być zaznaczone.',
-        'only_one_must_be_selected' => 'Tylko jedno pole :attribute musi być zaznaczone.'
+        'only_one_must_be_selected' => 'Tylko jedno pole :attribute musi być zaznaczone.',
     ],
 
 ];
