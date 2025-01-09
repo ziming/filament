f1b7c1af52dee464255cee9da26190bbfc29606a
chester <chestersykes1@gmail.com>
2025-01-09 21:59:46 +0800

    added stub

--- packages/tables/.stubs.php
+++ packages/tables/.stubs.php
@@ -167,6 +167,8 @@ namespace Livewire\Features\SupportTesting {
 
         public function removeTableFilters(): static {}
 
+        public function assertTableFilterExists(string $name, ?Closure $checkFilterUsing = null): static {}
+
         public function assertCanSeeTableRecords(array | Collection $records, bool $inOrder = false): static {}
 
         public function assertCanNotSeeTableRecords(array | Collection $records): static {}
