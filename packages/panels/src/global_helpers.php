b244eddbd3cbcf1932254b8c3868f5d3b010f78a
Erik Gaal <me@erikgaal.nl>
2025-02-06 14:02:05 +0100

    filament() helper return types

--- packages/panels/src/global_helpers.php
+++ packages/panels/src/global_helpers.php
@@ -4,6 +4,10 @@ use Filament\Contracts\Plugin;
 use Filament\FilamentManager;
 
 if (! function_exists('filament')) {
+    /**
+     * @param string|null $plugin
+     * @return ($plugin is null ? FilamentManager : Plugin)
+     */
     function filament(?string $plugin = null): FilamentManager | Plugin
     {
         /** @var FilamentManager $filament */
