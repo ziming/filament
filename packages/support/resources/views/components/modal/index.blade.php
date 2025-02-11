92e3264f3df3a381e8bf43baa05aba0de3f78257
Charlie Etienne <ce@web-nancy.fr>
2025-02-11 15:58:41 +0100

    Add a way to disable a modal trigger

--- packages/support/resources/views/components/modal/index.blade.php
+++ packages/support/resources/views/components/modal/index.blade.php
@@ -106,7 +106,9 @@
 >
     @if ($trigger)
         <div
-            x-on:click="open"
+            @if(!$trigger->attributes['disabled'] ?? false)
+                x-on:click="open"
+            @endif
             {{ $trigger->attributes->class(['fi-modal-trigger flex cursor-pointer']) }}
         >
             {{ $trigger }}
